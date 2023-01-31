cb52e28febcbc5e6789079207e55b4443144d162
971023als <971023als@naver.com>
2023-01-31 20:41:17 +0900

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -2,64 +2,64 @@
 
 . function.sh
 
-TMP1='SCRIPTNAME'.log
+TMP1=`SCRIPTNAME`.log
 
-> $TMP1
+> $TMP1 
 
-U-01.sh
+. U-01.sh
 
-U-07.sh
+. U-07.sh
 
-U-08.sh
+. U-08.sh
 
-U-08.sh
+. U-08.sh
 
-U-10.sh
+. U-10.sh
 
-U-11.sh
+. U-11.sh
 
-U-12.sh
+. U-12.sh
 
-U-13.sh
+. U-13.sh
 
-U-14.sh
+. U-14.sh
 
-U-19.sh
+. U-19.sh
 
-U-20.sh
+. U-20.sh
 
-U-21.sh
+. U-21.sh
 
-U-23.sh
+. U-23.sh
 
-U-24.sh
+. U-24.sh
 
-U-25.sh
+. U-25.sh
 
-U-26.sh
+. U-26.sh
 
-U-27.sh
+. U-27.sh
 
-U-28.sh
+. U-28.sh
 
-U-29.sh
+. U-29.sh
 
-U-31.sh
+. U-31.sh
 
-U-32.sh
+. U-32.sh
 
-U-35.sh
+. U-35.sh
 
-U-36.sh
+. U-36.sh
 
-U-37.sh
+. U-37.sh
 
-U-38.sh
+. U-38.sh
 
-U-39.sh
+. U-39.sh
 
-U-40.sh
+. U-40.sh
 
-U-41.sh
+. U-41.sh
 
-U-45.sh
+. U-45.sh
